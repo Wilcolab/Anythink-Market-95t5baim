b902369212280e40de49d00574cae780d2e98614
Caroline Vo <31779607+carolinevongoc@users.noreply.github.com>
2023-04-24 19:22:13 +0000

    correction

--- frontend/src/components/PageNotFound.js
+++ frontend/src/components/PageNotFound.js
@@ -3,7 +3,7 @@ import React from 'react'
 const PageNotFound = () => {
   return (
     <div style={{ backgroundColor: 'red' }}>
-      <h1>Page Not Foud </h1>
+      <h1> {' '} Page Not Found </h1>
       <p>Oops, Looks like you've stumbled upon an item that currently does not exist. But don't worry, there are plenty of other cool items to choose from in the Anythink marketplace. Take a look around and find something new!</p>
     </div>)
 }
