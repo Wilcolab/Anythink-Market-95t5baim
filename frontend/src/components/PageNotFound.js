43700daa0d68f52eb2344df4e7e1130fa275700e
Caroline Vo <31779607+carolinevongoc@users.noreply.github.com>
2023-04-25 15:12:45 +0000

    add a link return home in pageNotFound

--- frontend/src/components/PageNotFound.js
+++ frontend/src/components/PageNotFound.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+
 
 const PageNotFound = () => {
   return (
-    <div style={{ backgroundColor: 'red' }}>
-      <h1> {' '} Page Not Found </h1>
+    <><div style={{ backgroundColor: 'red' }}>
+      <h1> {' '} Page Not Found ! </h1>
       <p>Oops, Looks like you've stumbled upon an item that currently does not exist. But don't worry, there are plenty of other cool items to choose from in the Anythink marketplace. Take a look around and find something new!</p>
-    </div>)
+    </div><div >
+        <Link to="/"  style={{color: "red"}}>
+          Return Home
+        </Link></div></>
+  )
 }
 
 export default PageNotFound;
