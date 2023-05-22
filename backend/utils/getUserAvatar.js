7eccfbaad6aa4ac57c980876c9b1c8e55c3513fe
Caroline Vo <31779607+carolinevongoc@users.noreply.github.com>
2023-05-22 21:23:50 +0000

    add canvas and create file getUserAvatar.js

---
+++ backend/utils/getUserAvatar.js
@@ -0,0 +1,18 @@
+
+const { createCanvas, loadImage } = require('canvas');
+
+export const getUSerAvatar = (username) => {
+
+    const canvas = createCanvas(200, 200);
+    const ctx = canvas.getContext('2d');
+
+
+    ctx.font = '30px Impact'
+    ctx.rotate(0.1)
+    ctx.fillText(username[0] + username[1], 10, 150);
+
+
+    return canvas.toDataURL();
+}
+
+
