bafdb6d35d79c0942bbfe8e4a4f8175105e90669
Caroline Vo <31779607+carolinevongoc@users.noreply.github.com>
2023-05-23 12:16:17 +0000

    update image style

--- backend/utils/getUserAvatar.js
+++ backend/utils/getUserAvatar.js
@@ -1,14 +1,25 @@
-const { createCanvas, loadImage } = require('canvas');
+const { createCanvas } = require('canvas');
 
 const getUserAvatar = (username) => {
     const canvas = createCanvas(200, 200);
     const ctx = canvas.getContext('2d');
-
-    ctx.font = '30px Impact'
-    ctx.rotate(0.1)
-    ctx.fillText(username[0] + username[1], 10, 150);
-
+  
+    ctx.fillStyle = 'white'; // Set background color
+    ctx.fillRect(0, 0, canvas.width, canvas.height); // Draw background
+  
+    ctx.font = '60px Impact';
+    ctx.fillStyle = 'black';
+  
+    const text = (username[0] + username[1]).toUpperCase();
+    const textWidth = ctx.measureText(text).width; // Get the width of the text
+  
+    const x = (canvas.width - textWidth) / 2; // Calculate x-coordinate for centering
+    const y = (canvas.height + 60) / 2; // Calculate y-coordinate for centering
+  
+    ctx.fillText(text, x, y); // Draw the text at the center
+  
     return canvas.toDataURL();
 }
-
 module.exports = { getUserAvatar };
+
+
