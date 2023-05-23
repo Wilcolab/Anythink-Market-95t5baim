c47818a363593c5f149a42ce7f6bf66613cecabc
Caroline Vo <31779607+carolinevongoc@users.noreply.github.com>
2023-05-23 11:09:03 +0000

    correction : typo name and export module

--- backend/utils/getUserAvatar.js
+++ backend/utils/getUserAvatar.js
@@ -1,18 +1,14 @@
-
 const { createCanvas, loadImage } = require('canvas');
 
-export const getUSerAvatar = (username) => {
-
+const getUserAvatar = (username) => {
     const canvas = createCanvas(200, 200);
     const ctx = canvas.getContext('2d');
-
 
     ctx.font = '30px Impact'
     ctx.rotate(0.1)
     ctx.fillText(username[0] + username[1], 10, 150);
 
-
     return canvas.toDataURL();
 }
 
-
+module.exports = { getUserAvatar };
